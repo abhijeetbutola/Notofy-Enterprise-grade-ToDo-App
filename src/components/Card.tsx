2d03d7f97d0ba83debbe3c49143f45ff44395b2b
unknown <abhijeet.butola@gmail.com>
2024-03-24 12:47:39 +0530

    Initial commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Card.css";
+
+function Card() {
+  return (
+    <div
+      style={{ backgroundColor: "lightblue", height: "200px", width: "150px" }}
+    >
+      <p>Card</p>
+    </div>
+  );
+}
+
+export default Card;
